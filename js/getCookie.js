237135a87558ec478db6bd6623e3dd1e0320bad5
4xx404 <45526353+4xx404@users.noreply.github.com>
2022-03-26 21:10:33 +0000

    Delete getCookie.js

--- js/getCookie.js
+++
@@ -1,7 +0,0 @@
-function getCookie(){
-        var decodedCookie = decodeURIComponent(document.cookie);
-	var dc = decodedCookie;
-	document.getElementById('cookieval').value = document.cookie;
-	document.getElementById('decodedcookie').value = dc;
-	document.getElementById('submitForm').click();			
-}
