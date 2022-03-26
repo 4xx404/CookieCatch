deef8a47f896c4adbbb7434f4f75b177f13ea9ad
4xx404 <45526353+4xx404@users.noreply.github.com>
2022-03-26 21:10:22 +0000

    Delete db.php

--- db/db.php
+++
@@ -1,4 +0,0 @@
-<?php
-	$db = new SQLite3('db/CookieCatch.db');
-	$db->exec("CREATE TABLE IF NOT EXISTS catches(id INTEGER PRIMARY KEY AUTOINCREMENT, cookie TEXT, decoded_cookie TEXT, ip_address TEXT, hostname TEXT, device_details TEXT, catch_date TIMESTAMP)");
-?>
