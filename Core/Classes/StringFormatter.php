8d9d53c202ecbb1804ab13e9a9e70d25f2eacfef
4xx404 <45526353+4xx404@users.noreply.github.com>
2022-04-21 19:25:50 +0100

    Update StringFormatter.php

--- Core/Classes/StringFormatter.php
+++ Core/Classes/StringFormatter.php
@@ -13,9 +13,7 @@ class StringFormatter {
     }
     
     public static function BuildFullLocationString(string $City = null, string $Region = null, string $Country = null) {
-        $FullLocationString = "";
-
-        $FullLocationString .= ($City !== null) ? (($Region !== null) ? (($Country !== null) ? escape(ucwords(strtolower($City))) . ", " . escape(ucwords(strtolower($Region))) . ", " . escape(ucwords(strtolower($Country))) : escape(ucwords(strtolower($City))) . ", " . escape(ucwords(strtolower($Region)))) : escape(ucwords(strtolower($City)))) : "Unknown Location";
+        $FullLocationString = ($City !== null) ? (($Region !== null) ? (($Country !== null) ? escape(ucwords(strtolower($City))) . ", " . escape(ucwords(strtolower($Region))) . ", " . escape(ucwords(strtolower($Country))) : escape(ucwords(strtolower($City))) . ", " . escape(ucwords(strtolower($Region)))) : escape(ucwords(strtolower($City)))) : "Unknown Location";
 
         return $FullLocationString;
     }
